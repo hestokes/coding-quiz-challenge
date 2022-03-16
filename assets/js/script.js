1494fe6debc60be8c2c99256f147558a50fb649f
Aaron Stokes <stokescomp01@gmail.com>
2022-03-16 10:50:28 -0600

    add highscore verbiage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,7 @@ var choice1 = document.querySelector(".btn1");
 var choice2 = document.querySelector(".btn2");
 var choice3 = document.querySelector(".btn3");
 let score = 0;
+
+var highscore = localStorage.getItem("highscore");
+const scoreText = document.querySelector(".score");
+
