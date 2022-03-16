1946c915415c5c5df82c88d5740b50684febee53
Aaron Stokes <stokescomp01@gmail.com>
2022-03-16 10:47:13 -0600

    add variables for bhtml, add variables for buttons

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var menu = document.querySelector(".menu");
+
+var main = document.querySelector(".main");
+var quiz = document.querySelector(".quiz");
+
+var choice0 = document.querySelector(".btn0");
+var choice1 = document.querySelector(".btn1");
+var choice2 = document.querySelector(".btn2");
+var choice3 = document.querySelector(".btn3");
+let score = 0;
