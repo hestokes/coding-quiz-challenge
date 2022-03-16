c1832f592ae0cd85312dbb435b8d2f8ee899a7de
Aaron Stokes <stokescomp01@gmail.com>
2022-03-16 10:52:40 -0600

    add starting function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,32 @@ let score = 0;
 var highscore = localStorage.getItem("highscore");
 const scoreText = document.querySelector(".score");
 
+var start = function () {
+  //Start and introduction
+  var introContainer = document.createElement("div");
+  var h1El = document.createElement("h1");
+  var pEl = document.createElement("p");
+  var buttonEl = document.createElement("button");
+
+  h1El.textContent = "Coding Quiz Challenge";
+  pEl.textContent =
+    "Try to answer the following code-relrated questions within the time limit. Keep in mind that incorrect answers will penaltize your score time by 10 seconds";
+  buttonEl.textContent = "Start Quiz";
+
+  introContainer.className = "intro-main";
+  h1El.className = "intro-title";
+  pEl.className = "intro-info";
+  buttonEl.className = "start-button";
+
+  main.appendChild(introContainer);
+  introContainer.appendChild(h1El);
+  introContainer.appendChild(pEl);
+  introContainer.appendChild(buttonEl);
+
+  buttonEl.addEventListener("click", function () {
+    introContainer.remove();
+    timeCount();
+  });
+};
+
+//Add quiz Questions TODO
